44787d6faae5f4fb48e969dc3cd653badc311517
Felix Cadieux <cadieuxfelix@outlook.com>
2025-05-08 19:37:15 -0400

    Cors setup for 4200

--- src/main/java/com/api/route_calculator/rest/RouteRestController.java
+++ src/main/java/com/api/route_calculator/rest/RouteRestController.java
@@ -3,13 +3,10 @@ package com.api.route_calculator.rest;
 import com.api.route_calculator.entity.route.Route;
 import com.api.route_calculator.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class RouteRestController {
